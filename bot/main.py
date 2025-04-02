e46cbc8be886882c193fd8ace64591a041cf4f4c
million-art <millionmulugeta09@gmail.com>
2025-04-02 11:05:57 +0300

    fix webhook

--- bot/main.py
+++ bot/main.py
@@ -2,36 +2,35 @@ from dotenv import load_dotenv
 import os
 from telegram.ext import Application, CommandHandler, MessageHandler, filters
 from bot.handlers.ban_handler import check_full_name_and_ban
-from http.server import BaseHTTPRequestHandler
+from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 import asyncio
+from telegram import Update
+import logging
 
-def main():
-    # Load environment variables
-    load_dotenv()
-    token = os.getenv('BOT_TOKEN')
-    
-    if not token:
-        raise ValueError("No BOT_TOKEN found in environment variables")
-    
-    # Create the Application
-    application = Application.builder().token(token).build()
-    
-    # Add message handler
-    application.add_handler(
-        MessageHandler(filters.ALL & ~filters.COMMAND, check_full_name_and_ban)
-    )
-    
-    # Add command handler
-    application.add_handler(CommandHandler('start', start))
-    
-    # Start the bot
-    application.run_polling()
+# Load environment variables
+load_dotenv()
+token = os.getenv('BOT_TOKEN')
 
+if not token:
+    raise ValueError("No BOT_TOKEN found in environment variables")
+
+# Create the Application
+application = Application.builder().token(token).build()
+
+# Add message handler
+application.add_handler(
+    MessageHandler(filters.ALL & ~filters.COMMAND, check_full_name_and_ban)
+)
+
+# Add command handler
+application.add_handler(CommandHandler('start', start))
+
+ 
 async def start(update, context):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Hello! I am group manager bot.")
 
-# HTTP handler for Vercel
+# HTTP handler for incoming webhook requests
 class handler(BaseHTTPRequestHandler):
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
@@ -44,11 +43,23 @@ class handler(BaseHTTPRequestHandler):
         self.end_headers()
 
     async def process_update(self, update_dict):
-        update = types.Update.de_json(update_dict)
-        await bot.process_new_updates([update])
+        update = Update.de_json(update_dict, application.bot)
+        await application.process_update(update)
 
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write('Hello, BOT is running!'.encode('utf-8'))
-  
+        self.wfile.write(b'Hello, BOT is running!')
+
+def run_server(server_class=HTTPServer, handler_class=handler, port=8080):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    logger.info(f"Starting httpd server on port {port}")
+    httpd.serve_forever()
+
+def main():
+    # Start the HTTP server to handle incoming webhook requests
+    run_server()
+
+if __name__ == "__main__":
+    main()
